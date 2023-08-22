a028da1bf1a7299b1cafb8cf5617a1261b71873b
Gareth Fultz <garethfultz@gmail.com>
2023-08-21 22:49:46 -0400

    Updated blog and resume

--- src/modules/blog/PageList.tsx
+++ src/modules/blog/PageList.tsx
@@ -10,32 +10,8 @@ export type BlogPage = {
     location: string;
 };
 
-//module.exports=require(__dirname +'/pageDataFetch.js');
 const rawPages: any[] = preval`
-const fs = require('fs');
-const path = require('path');
-const frontmatter = require('@github-docs/frontmatter')
-
-const blogPageDir = path.join('public', 'assets', 'blog', 'pages');
-const fileNames = fs.readdirSync(blogPageDir);
-
-const fileContents = []
-for (const fileName of fileNames) {
-    const fileLocation = path.join(blogPageDir, fileName);
-    let data = "none"
-    try {
-        const { data, _ } = frontmatter(fs.readFileSync(fileLocation, 'utf8'));
-        fileContents.push({
-            id: fileName,
-            ...data,
-            location: fileName
-        });
-    } catch (err) {
-        fileContents.push(err);
-    }
-}
-
-module.exports = fileContents;
+    module.exports=require(__dirname +'/pageDataFetch.js');
 `;
 
 const pages: BlogPage[] = [];
